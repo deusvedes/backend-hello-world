8de6d3c4ac144ab959e7a16474c42c0e4ba95a4c
Nico Weinberg <nico.weinberg@gmail.com>
2022-12-13 16:27:42 +0100

    initial

--- src/main/java/com/swisscom/taaweni0/demoapp/controller/WorldController.java
+++ src/main/java/com/swisscom/taaweni0/demoapp/controller/WorldController.java
@@ -1,17 +1,30 @@
 package com.swisscom.taaweni0.demoapp.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 @Slf4j
 public class WorldController {
+    String host = "https://taaweni0-hello-world-hello-world.azuremicroservices.io";
     @GetMapping("/hello")
-    public String hello() {
+    public ResponseEntity<String> hello() {
         log.info("hello called successfully");
-        return "Hello World at: " + LocalDateTime.now();
+        String response = "Hello World at: " + LocalDateTime.now();
+        MultiValueMap<String, String> responseHeaders = new LinkedMultiValueMap<String, String>();
+        responseHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, host);
+        return new ResponseEntity<>(response, responseHeaders, HttpStatus.OK);
     }
 }
